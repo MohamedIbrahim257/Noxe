aed602a122d88fe48c6eaafc067e0754094719a9
Mohamed Ibrahim <86324885+MohamedIbrahim257@users.noreply.github.com>
2023-03-19 19:39:21 +0200

    Register

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
    const  getMovies = async (mediaType , callBack) => {
     let {data}= await axios.get(`https://api.themoviedb.org/3/${callBack}/${mediaType}/day?api_key=eba8b9a7199efdcb0ca1f96879b83c44`)
 
-
+     
   }
   return (
     <div ></div>
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,89 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import Joi from 'joi'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Joi from 'joi';
 
 export default function Register() {
-
     let navigate = useNavigate()
-    let [errorReg, setErrorReg] = useState([])
-    let [error, setError] = useState("")
-
-    let [user, setUserData] = useState({
-        Name: "",
+    const [error, setError] = useState("");
+    const [errorValidate, setErrorValidate] = useState([])
+    const [user, setUserData] = useState({
+        first_Name: "",
         Age: 0,
         email: "",
-        Password: ""
+        password: ""
     })
 
-    function submitFormData(e) {
-        e.preventDefault()
-        let validation = validationReg()
-        if (validation.error) {
-            setError(validation.error.message)
-        } else {
-
-           localStorage.setItem("setUsersData", JSON.stringify(user))
-
-            //   await   axios.post('https://jsonplaceholder.typicode.com/posts', {
-            //         headers: { 'Content-Type': 'application/json' },
-            //         body: {
-            //             user
-            //         }
-            //     })
-            //         .then(response => {
-            //             console.log(response.data);
-            //             const token = response.data.id;
-            //             return token;
-            //         })
-            //         .catch(error => {
-            //             console.log(error); // You can handle error response here
-            //         });
-
-            navigate('/Login')
-
-        }
-
+    const getUserData = (e) => {
+        let myUsers = { ...user };
+        myUsers[e.target.name] = e.target.value;
+        setUserData(myUsers)
+        console.log(myUsers);
     }
 
-    function validationReg() {
+    const submitUsersForm = async (e) => {
+        e.preventDefault()
+        let validation = validationForm()
+        if (validation.error) {
+            setErrorValidate(validation.error.message)
+        } else {
+            let { data } = await axios.post(`https://route-egypt-api.herokuapp.com//signup`, user)
+            if (data.message === 'success') {
+                navigate("/Login")
+            } else {
+                setError(data.message)
+            }
+        }
+    }
+
+    const validationForm = () => {
         let schema = Joi.object({
-            Name: Joi.string().alphanum().min(3).max(10).required(),
-            Age: Joi.number().min(16).max(80).required(),
+            first_Name: Joi.string().alphanum().min(3).max(20).required(),
+            Age: Joi.number().min(16).max(60).required(),
             email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'org'] } }).required(),
-            Password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
         })
         return schema.validate(user)
     }
 
-
-    function getUserData(e) {
-        let userData = { ...user }
-        userData[e.target.name] = e.target.value
-        setUserData(userData)
-        console.log(userData)
-
-    }
-
     return (
-        <div className='my-5' >
-            <form onSubmit={submitFormData} >
-                <h2 className='my-5' >Register</h2>
-                <div className='w-75 mx-auto' >
-                    {errorReg.length > 0 ? <div className='alert alert-danger' >{errorReg}</div> : ""}
-                    {error.length > 0 ? <div className='alert alert-danger' >{error}</div> : ""}
-                    <label htmlFor="email">Name</label>
-                    <input onChange={getUserData} className='form-control my-2' type="text" id='Name' name='Name' />
-                    <label htmlFor="age">Age</label>
-                    <input onChange={getUserData} className='form-control my-2' type="age" id='age' name='Age' />
-                    <label htmlFor="email">Email</label>
-                    <input onChange={getUserData} className='form-control my-2' type="email" id='email' name='email' />
-                    <label htmlFor="current-password">Password</label>
-                    <input onChange={getUserData} className='form-control my-2' type="password" id='password' name='Password' />
-                    <div className='my-2'>
-                        <button className='btn btn-outline-info my-2' >Register</button>
-                    </div>
-                </div>
+        <div>
+            <h2 className='my-4' >Register</h2>
+            <form onSubmit={submitUsersForm} className='my-5' >
+                {error.length > 0 ? <div className='alert alert-danger'>{error}</div> : ""}
+                {errorValidate.length > 0 ? <div className='alert alert-danger'>{errorValidate}</div> : ""}
+                <label htmlFor="first_Name">First Name</label>
+                <input onChange={getUserData} className='form-control my-2' id='first_Name' type="text" name='first_Name' />
+                <label htmlFor="Age">age</label>
+                <input onChange={getUserData} className='form-control my-2' name='Age' id='Age' type="number" />
+                <label htmlFor="email">email</label>
+                <input onChange={getUserData} className='form-control my-2' name='email' id='email' type="email" />
+                <label htmlFor="password">password</label>
+                <input onChange={getUserData} className='form-control my-2' name='password' id='Age' type="password" />
+                <button className='btn btn-outline-info my-2' type='submit' >Register</button>
             </form>
         </div>
     )
